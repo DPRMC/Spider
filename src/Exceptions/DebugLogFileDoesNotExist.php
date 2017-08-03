3f3d1d084df718cd3ec4c9ff5792158086c922e8
Michael Drennen <michaeldrennen74@gmail.com>
2017-08-03 11:27:58 -0600

    Added a couple of new Exceptions.

---
+++ src/Exceptions/DebugLogFileDoesNotExist.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DPRMC\Spider\Exceptions;
+
+use Exception;
+
+class DebugLogFileDoesNotExist extends Exception {
+
+}
---
+++ src/Exceptions/ReadMeFileDoesNotExists.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DPRMC\Spider\Exceptions;
+
+use Exception;
+
+class ReadMeFileDoesNotExists extends Exception {
+
+}
