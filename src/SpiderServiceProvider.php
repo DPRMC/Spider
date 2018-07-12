7a9372a480c1e9d817a083bcb46dcdf8d2e5e923
Michael Drennen <michaeldrennen74@gmail.com>
2018-07-12 13:50:00 -0600

    Added the ServiceProvider

---
+++ src/SpiderServiceProvider.php
@@ -0,0 +1,68 @@
+<?php namespace Dprc\Spider;
+
+use Illuminate\Support\ServiceProvider;
+
+class SpiderServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('dprc/spider');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+
+        $this->app->booting(function(){
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Spider','Dprc\Spider\Facades\Spider');
+        });
+
+
+        $this->app['spider'] = $this->app->share(function($app)
+        {
+            return new Spider;
+        });
+
+        $this->app['step'] = $this->app->share(function($app)
+        {
+            return new Step;
+        });
+
+        $this->app['failure_rule'] = $this->app->share(function($app)
+        {
+            return new FailureRule;
+        });
+
+        $this->app['success_rule'] = $this->app->share(function($app)
+        {
+            return new SuccessRule;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('spider');
+    }
+
+}
